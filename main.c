2bfb5863d3efddd09fdc7b6a9bf565f6bcd569f8
Andrew Hett <andrewmhett@gmail.com>
2021-02-13 15:59:37 -0800

    Added swapping and pivoting to rref_matrix()

--- main.c
+++ main.c
@@ -1,24 +1,25 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include "matrix.h"
 
-void draw_matrix(int ** matrix, int rows, int cols){
+void draw_matrix(float ** matrix, int rows, int cols){
 	int col_widths[cols];
 	for (int i=0;i<rows;i++){
 		for (int o=0;o<cols;o++){
 			col_widths[o]=0;
 			char str_value[10];
-			sprintf(str_value, "%d", matrix[i][o]);
+			sprintf(str_value, "%f", matrix[i][o]);
 			if (strlen(str_value)>col_widths[o]){
 				col_widths[o]=(int)strlen(str_value);
 			}
 		}
-	}	
+	}
 	for (int i=0;i<rows;i++){
 		for (int o=0;o<cols;o++){
 			char str_value[10];
-			sprintf(str_value, "%d", matrix[i][o]);
-			printf("%d",matrix[i][o]);
+			sprintf(str_value, "%f", matrix[i][o]);
+			printf("%.2f",matrix[i][o]);
 			for (int n=-1;n<col_widths[o]-(int)strlen(str_value);n++){
 				printf(" ");
 			}
@@ -27,8 +28,39 @@ void draw_matrix(int ** matrix, int rows, int cols){
 	}
 }
 
-void rref_matrix(int ** original_matrix, int row, int cols){
-	
+
+
+void rref_matrix(float ** matrix, int rows, int cols){
+	for (int i=0;i<rows;i++){
+		if (matrix[i][0] != 0){
+			if (i == 0){
+				break;
+			}else{
+				swap_rows(matrix, i, 0);
+				printf("R%d <-> R1\n", i+1);
+				draw_matrix(matrix, rows, cols);
+				break;
+			}
+		}
+	}
+	if (matrix[0][0] != 1){
+		printf("R1 / %.2f\n", matrix[0][0]);
+		normalize_row(matrix, 0, cols);
+		draw_matrix(matrix, rows, cols);
+	}
+	for (int i=1;i<rows;i++){
+		if (matrix[i][0] != 0){
+			printf("R%d - %.2f * R1\n", i+1, matrix[i][0]);
+			float buffer[cols];
+			for (int o=0;o<cols;o++){
+				buffer[o] = matrix[i][o];
+			}
+			for (int o=0;o<cols;o++){
+				matrix[i][o] -= (buffer[0]*matrix[0][o]);
+			}
+			draw_matrix(matrix, rows, cols);
+		}	
+	}
 }
 
 int main(){
@@ -38,14 +70,14 @@ int main(){
 	printf("How many columns? ");
 	scanf("%d", &cols);
 	printf("Solving for %dx%d matrix...\n",rows,cols);
-	int ** matrix_array = malloc(rows*sizeof(int*));
+	float ** matrix_array = malloc(rows*sizeof(int*));
 	for (int i=0;i<rows;i++){
 		matrix_array[i] = malloc(cols*sizeof(int));
 	}
 	for (int i=0;i<rows;i++){
 		for (int o=0;o<cols;o++){
 			printf("Row %d, Column %d: ",i+1,o+1);
-			scanf("%d", &matrix_array[i][o]);
+			scanf("%f", &matrix_array[i][o]);
 		}
 	}
 	printf("INITIAL MATRIX\n");
